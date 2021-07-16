e46c4665ab50bfc9377cb58fd38e7ba1117ac0d9
Minerva <rezaiealireza884@gmail>
2021-07-16 14:40:25 +0430

    initial commit

---
+++ index.js
@@ -0,0 +1,41 @@
+require("dotenv").config();
+
+const deepai = require("deepai");
+deepai.setApiKey(process.env.NFSW_API_KEY);
+
+const Discord = require("discord.js");
+const client = new Discord.Client();
+
+async function is_nude(img_url) {
+  let resp = await deepai.callStandardApi("nsfw-detector", {
+    image: img_url.toString(),
+  });
+  let output = [];
+  resp.output.detections.forEach((element) => {
+    output.push(element.name);
+  });
+
+  return output === [] ? undefined : output.join(",");
+}
+client.on("ready", () => {
+  console.log("bot is ready");
+});
+
+client.on("message", (msg) => {
+  try {
+    recievedImgUrl = msg.attachments.first().url;
+
+    is_nude(recievedImgUrl)
+      .then((result) => {
+        if (result) {
+          msg.reply("بی ادب \n" + result);
+          console.log(result);
+        }
+      })
+      .catch(() => console.log("error"));
+  } catch (e) {
+    console.log("not an image");
+  }
+});
+
+client.login(process.env.BOT_TOKEN);
