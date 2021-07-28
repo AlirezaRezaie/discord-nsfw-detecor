6cdc871ade54acb4e514ae2e5b78648160d2cc52
Minerva <rezaiealireza884@gmail>
2021-07-29 01:03:20 +0430

    added new token

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const Client = require("./gitwatch");
 const requestEndPoint = "https://api.github.com/repos/Minerova/SCWEC/commits";
-const github_token = "token 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";
+const github_token = "token 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";
 let gitwatchClient = new Client.Detector(requestEndPoint, github_token);
 const deepai = require("deepai");
 
